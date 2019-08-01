cc8c41e70bacff918009f4417522eec953b5bf49
2050531502 <2050531502@qq.com>
2019-08-01 21:09:10 +0800

    项目正常运行

--- gulimall-admin/src/main/java/com/atguigu/GmallAdminMainApplication.java
+++ gulimall-admin/src/main/java/com/atguigu/GmallAdminMainApplication.java
@@ -8,11 +8,14 @@
 
 package com.atguigu;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-
+@EnableSwagger2
 @SpringBootApplication
+@MapperScan(basePackages = "com.atguigu.gulimall.admin.dao")
 public class GmallAdminMainApplication {
 
 	public static void main(String[] args) {
--- gulimall-oms/src/main/java/com/atguigu/gulimall/oms/GulimallOmsApplication.java
+++ gulimall-oms/src/main/java/com/atguigu/gulimall/oms/GulimallOmsApplication.java
@@ -1,13 +1,17 @@
 package com.atguigu.gulimall.oms;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
-public class GulimallOmsApplication {
+@MapperScan(basePackages = "com.atguigu.gulimall.oms.dao")
+public class GulimallomsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GulimallOmsApplication.class, args);
+        SpringApplication.run(GulimallomsApplication.class, args);
     }
 
 }
